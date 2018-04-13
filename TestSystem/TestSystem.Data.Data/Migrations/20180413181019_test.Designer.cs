08ae2f2cf5265a944cee7f2634bfce3d02f7f312
Jivka Ignatova <ignatova.jivka@gmail.com>
2018-04-13 21:16:04 +0300

    small changes

--- TestSystem/TestSystem.Data.Data/Migrations/20180413181019_test.Designer.cs
+++ TestSystem/TestSystem.Data.Data/Migrations/20180413181019_test.Designer.cs
@@ -11,9 +11,10 @@ using TestSystem.Web.Data;
 namespace TestSystem.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180413181019_test")]
+    partial class test
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
