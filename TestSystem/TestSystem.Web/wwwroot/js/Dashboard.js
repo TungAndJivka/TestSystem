e4a3cf7f4cef0439ba0cee6cad443e837d40c32b
Unknown <dokhanhtung1993@gmail.com>
2018-04-21 15:37:21 +0300

    Covered Pagination requirement

--- TestSystem/TestSystem.Web/wwwroot/js/Dashboard.js
+++ TestSystem/TestSystem.Web/wwwroot/js/Dashboard.js
@@ -3,6 +3,16 @@
 $(document).ready(function () {
     $('#example').DataTable(
         {
-            "pagingType": "full_numbers"
-        });
+            "pagingType": "full_numbers",
+            "fnDrawCallback": function () {
+                var $paginate = this.siblings('.dataTables_paginate');
+
+                if (this.api().data().length <= this.fnSettings()._iDisplayLength) {
+                    $paginate.hide();
+                }
+                else {
+                    $paginate.show();
+                }
+            }
+        })
 });
