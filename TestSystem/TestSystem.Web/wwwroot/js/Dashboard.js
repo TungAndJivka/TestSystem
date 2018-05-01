244238663d44d2c3441f7875c4a122792028ba22
Jivka Ignatova <ignatova.jivka@gmail.com>
2018-05-01 22:31:53 +0300

    bug fix

--- TestSystem/TestSystem.Web/wwwroot/js/Dashboard.js
+++ TestSystem/TestSystem.Web/wwwroot/js/Dashboard.js
@@ -1,5 +1,9 @@
-﻿$(function () {
-    $('#Java').click();
+﻿
+$(document).ready(function () {
+    //$('#Java').click();
+    //$('#Java').attr("aria-expanded", true);
+    //console.log('tab click');
+    $('.nav-item a').first().click();
 });
 
 
