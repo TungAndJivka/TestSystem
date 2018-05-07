720dfab17144637fa9e180db00867eb0ff88eefb
Unknown <dokhanhtung1993@gmail.com>
2018-05-07 23:42:24 +0300

    chart.js WIP

--- TestSystem/TestSystem.Web/wwwroot/js/Dashboard.js
+++ TestSystem/TestSystem.Web/wwwroot/js/Dashboard.js
@@ -45,4 +45,50 @@ const categoryTabClickSubmitted = (function (categoryName) {
     $('#DashboardContent').append(row);
     $('#testName').html(testName);
 });
+var ctx = document.getElementById("myChart").getContext('2d');
 
+var teststatistics;
+
+$(function () {
+    var $this = $(this);
+    var frmValues = $this.serialize();
+    $.ajax({
+        type: $this.attr('get'),
+        url: $this.attr('/Dashboard/GetAA'),
+    })
+        .done(function (Json) {
+            teststatistics = Json;
+        })
+        .fail(function () {
+            $("#para").text("An error occured");
+        });
+})
+
+
+
+var myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ["Java", "SQL", ".NET", "JavaScript"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',   
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        
+    }
+});
